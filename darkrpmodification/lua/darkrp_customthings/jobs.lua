182d6950ee6abdf63e8e032de2fe0b8736f3d80f
PhoenixFuture <120593437+PhoenixFuture@users.noreply.github.com>
2022-12-23 20:59:03 +0100

    Update jobs.lua

--- darkrpmodification/lua/darkrp_customthings/jobs.lua
+++ darkrpmodification/lua/darkrp_customthings/jobs.lua
@@ -293,12 +293,12 @@ TEAM_ARC_CAPTAIN = DarkRP.createJob("ARC Trooper │ Captain", {
     end,
 })
 
-TEAM_ARC_1STLT1 = DarkRP.createJob("ARC Trooper │ 1st Lieutenant 1", {
+TEAM_ARC_1STLT = DarkRP.createJob("ARC Trooper │ 1st Lieutenant", {
     color = Color(153, 0, 0),
     model = {"models/mks/custom/drift/drift.mdl", "models/mks/custom/drift/drift.mdl", "models/mks/custom/muun/muun.mdl", "models/sky/custom/meister/meister.mdl",  "models/mks/custom/tracer/tracer.mdl", "models/defcon/stan/arcevo/rancor/evorancor.mdl", "models/starwars/grady/legends_arcs/legends_arc_rino.mdl", "models/starwars/grady/legends_arcs/legends_arc_grady.mdl", "models/starwars/grady/legends_arcs/legends_arc_lieutenant.mdl", "models/starwars/grady/legends_arcs/legends_arc_sergeant.mdl", "models/starwars/grady/legends_arcs/legends_arc_trooper.mdl", "models/starwars/grady/legends_arcs/legends_arc_commander.mdl" , "models/sky/custom/sky/nexu.mdl"},
     description = [[]],
     weapons = {"rw_sw_westarm5", "weapon_bactainjector", "alydus_fusioncutter", "weapon_armorkit", "stunstick", "rw_sw_wristflame", "rw_sw_dual_dc17s", "rw_sw_nade_bacta", "rw_sw_nade_thermal", "rw_sw_nade_impact", "rw_sw_plx1", "rw_sw_valken38x", "jet_mk5", "shadow", "seal6-c4", "revival_tool", "weapon_cuff_elastic", "rw_sw_dual_dc15s"},
-    command = "give_job_arc_1stlt1",
+    command = "give_job_arc_1stlt",
     max = 0,
     salary = 100,
     admin = 0,
@@ -314,33 +314,12 @@ TEAM_ARC_1STLT1 = DarkRP.createJob("ARC Trooper │ 1st Lieutenant 1", {
     end,
 })
 
-TEAM_ARC_LT2 = DarkRP.createJob("ARC Trooper │ Lieutenant 2", {
+TEAM_ARC_LT = DarkRP.createJob("ARC Trooper │ Lieutenant", {
     color = Color(153, 0, 0),
     model = {"models/mks/custom/drift/drift.mdl", "models/mks/custom/drift/drift.mdl", "models/mks/custom/muun/muun.mdl", "models/sky/custom/meister/meister.mdl",  "models/mks/custom/tracer/tracer.mdl", "models/defcon/stan/arcevo/rancor/evorancor.mdl", "models/starwars/grady/legends_arcs/legends_arc_rino.mdl", "models/starwars/grady/legends_arcs/legends_arc_grady.mdl", "models/starwars/grady/legends_arcs/legends_arc_lieutenant.mdl", "models/starwars/grady/legends_arcs/legends_arc_sergeant.mdl", "models/starwars/grady/legends_arcs/legends_arc_trooper.mdl", "models/starwars/grady/legends_arcs/legends_arc_commander.mdl" , "models/sky/custom/sky/nexu.mdl"},
     description = [[]],
     weapons = {"rw_sw_westarm5", "weapon_bactainjector", "alydus_fusioncutter", "weapon_armorkit", "stunstick", "rw_sw_wristflame", "rw_sw_dual_dc17s", "rw_sw_nade_bacta", "rw_sw_nade_thermal", "rw_sw_nade_impact", "rw_sw_plx1", "rw_sw_valken38x", "jet_mk5", "shadow", "seal6-c4", "revival_tool", "weapon_cuff_elastic", "rw_sw_dual_dc15s"},
-    command = "give_job_arc_1stlt2",
-    max = 0,
-    salary = 100,
-    admin = 0,
-    vote = false,
-    hasLicense = false,
-    category = "Alpha Advanced Recon Commando",
-    canDemote = false,
-    PlayerSpawn = function(ply)
-        ply:SetHealth(300)
-        ply:SetMaxHealth(300)
-        ply:SetArmor(300)
-        ply:SetMaxArmor(300)
-    end,
-})
-
-TEAM_ARC_LT1 = DarkRP.createJob("ARC Trooper │ Lieutenant 1", {
-    color = Color(153, 0, 0),
-    model = {"models/mks/custom/drift/drift.mdl", "models/mks/custom/drift/drift.mdl", "models/mks/custom/muun/muun.mdl", "models/sky/custom/meister/meister.mdl",  "models/mks/custom/tracer/tracer.mdl", "models/defcon/stan/arcevo/rancor/evorancor.mdl", "models/starwars/grady/legends_arcs/legends_arc_rino.mdl", "models/starwars/grady/legends_arcs/legends_arc_grady.mdl", "models/starwars/grady/legends_arcs/legends_arc_lieutenant.mdl", "models/starwars/grady/legends_arcs/legends_arc_sergeant.mdl", "models/starwars/grady/legends_arcs/legends_arc_trooper.mdl", "models/starwars/grady/legends_arcs/legends_arc_commander.mdl" , "models/sky/custom/sky/nexu.mdl"},
-    description = [[]],
-    weapons = {"rw_sw_westarm5", "weapon_bactainjector", "alydus_fusioncutter", "weapon_armorkit", "stunstick", "rw_sw_wristflame", "rw_sw_dual_dc17s", "rw_sw_nade_bacta", "rw_sw_nade_thermal", "rw_sw_nade_impact", "rw_sw_plx1", "rw_sw_valken38x", "jet_mk5", "shadow", "seal6-c4", "revival_tool", "weapon_cuff_elastic", "rw_sw_dual_dc15s"},
-    command = "give_job_arc_lt1",
+    command = "give_job_arc_lt",
     max = 0,
     salary = 100,
     admin = 0,
